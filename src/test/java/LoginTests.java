4fb5967a4b12e1a1a4322ed8c9f117a8d679088f
anarouz86 <154649347+Anarouz1986@users.noreply.github.com>
2024-01-04 01:21:47 -0500

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -10,7 +10,7 @@ import java.time.Duration;
 
 public class LoginTests extends BaseTest {
     @Test
-    public void loginEmptyEmailPassword() {
+    public void navigateToKoelApp() {
 
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
@@ -25,6 +25,7 @@ public class LoginTests extends BaseTest {
         driver.quit();
     }
 }
+<<<<<<< Updated upstream
 @Test
 public void registrationNavigation() {
     // Added ChromeOptions argument below to fix websocket error
@@ -46,3 +47,73 @@ public void registrationNavigation() {
 
 public void main() {
 }
+=======
+
+@Test
+public void loginValidEmailPassword() {
+    //PreCondition
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origins=*");
+
+    //Manage Browser - wait for 10 seconds before failing/quitting.
+    WebDriver driver = new ChromeDriver(options);
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    //Steps
+    String url = "https://qa.koel.app/";
+    driver.get(url);
+
+    //Email Field
+    WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+    emailField.clear();
+    emailField.sendKeys("demo@class.com");
+    //Password Field
+    WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+    passwordField.clear();
+    passwordField.sendKeys("te$t$tudent");
+    //Submit button
+    WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+    submitButton.click();
+
+    //Assertion
+    WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+    Assert.assertTrue(avatarIcon.isDisplayed());
+
+    //Quit
+    driver.quit();
+}
+
+@Test
+public void loginWithInvalidEmailValidPassword() throws InterruptedException {
+    //PreCondition
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origins=*");
+
+    //Manage Browser - wait for 10 seconds before failing/quitting.
+    WebDriver driver = new ChromeDriver(options);
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    //Steps
+    String url = "https://qa.koel.app/";
+    driver.get(url);
+
+    //Email Field
+    WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+    emailField.clear();
+    emailField.sendKeys("invalid@class.com");
+    //Password Field
+    WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+    passwordField.clear();
+    passwordField.sendKeys("te$t$tudent");
+    //Submit button
+    WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+    submitButton.click();
+
+    Thread.sleep(2000);
+    //Expected Result
+    Assert.assertEquals(driver.getCurrentUrl(), url);
+
+    //close Browser
+    driver.quit();
+}
+>>>>>>> Stashed changes
